21280b43db9e8e41ba37b42f7112f26c58567240
Ashwin Chatterji <ashwinath@hotmail.com>
2018-02-04 17:09:18 +0800

    added restart script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 yarn build
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress . chat@192.168.1.199:/opt/apps/home-dashboard-spa;
+ssh chat@192.168.1.199 "pm2 restart home-dashboard-spa"
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const serve = require('serve');
 const path = require('path');
 
 const path_to_html = path.join(__dirname, 'build')
-console.log(path_to_html)
+
 serve(path_to_html, {
   port: 3000,
   ignore: ['node_modules']
